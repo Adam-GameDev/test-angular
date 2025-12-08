cf0f71e732c1712e07e2b83b097d00f1a755bb03
Adam-GameDev <137739735+Adam-GameDev@users.noreply.github.com>
2025-12-08 11:24:04 -0500

    Initial commit

---
+++ src/app/app.config.ts
@@ -0,0 +1,31 @@
+import {
+  ApplicationConfig,
+  provideZonelessChangeDetection,
+} from '@angular/core';
+import {provideRouter, withHashLocation} from '@angular/router';
+import {routes} from './app.routes';
+import {provideHttpClient} from '@angular/common/http';
+
+// Custom date formats
+export const CUSTOM_DATE_FORMATS = {
+  parse: {
+    dateInput: ['dd-MMM-yyyy'],
+  },
+  display: {
+    dateInput: 'dd-MMM-yyyy',
+    dateLabel: 'dd-MMM-yyyy',
+    dateTimeLabel: 'dd-MMM-yyyy HH:mm',
+    locale: 'en-US',
+    monthYearLabel: 'MMM yyyy',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM yyyy',
+  }
+}
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZonelessChangeDetection(),
+    provideRouter(routes, withHashLocation()),
+    provideHttpClient()
+  ]
+};
